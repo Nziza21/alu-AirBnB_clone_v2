11515b2debb11c548d976d596622c4fcb479bc0a
Nziza21 <n.samuel@alustudent.com>
2026-06-05 16:34:26 +0200

    Fix session to use scoped_session proxy directly

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -65,12 +65,12 @@ class DBStorage:
             self.__session.delete(obj)
 
     def reload(self):
-        """Create all tables and a new database session"""
-        Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
+     """Create all tables and a new database session"""
+     Base.metadata.create_all(self.__engine)
+     session_factory = sessionmaker(bind=self.__engine,
+                                   expire_on_commit=False)
+     Session = scoped_session(session_factory)
+     self.__session = Session
 
     def close(self):
         """Close the current session"""
